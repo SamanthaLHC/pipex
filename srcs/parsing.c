2d69c0fd9c33febc705b571bb47da915a7d2f1fe
Samantha LE HUEC <sle-huec@e2r11p21.clusters.42paris.fr>
2022-05-25 11:15:09 +0200

    add func open fd

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,16 +6,24 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 15:50:04 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/05/13 17:22:23 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/05/18 14:29:25 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	get_fd()
+int	get_fd(char **input)
 {
+	int	fd1;
+	int	fd2;
+
+	fd1 = open(input[1], O_RDONLY);
+	fd2 = open(input[4], O_WRONLY | O_CREAT | O_TRUNC, 644);
 	// open les files pour avoir les fd
-	// mettre les fd dans un tab ?
+	// mettre les fd dans un ta  b ?
+	// gestion d erreur sii le fichier 1 n existe pas ou si le files 
+	//ou si le file2 a deja des droits (autre que 644)
+	
 }
 
 char	**get_path()
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sle-huec <sle-huec@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:50:55 by sle-huec          #+#    #+#             */
-/*   Updated: 2022/05/13 17:05:55 by sle-huec         ###   ########.fr       */
+/*   Updated: 2022/05/18 14:32:23 by sle-huec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,5 @@ int main (int ac, char **av, char **env)
 {
 	if (ac != 5)
 		perror("pipex usage: file1 cmd1 | cmd2 file 2");
+		write(2, av[1], ft_strlen(av[1]));
 }
